3a42de57202ae5045d55756482bfcda32f28d81a
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-07-08 20:19:56 +0900

    created restaurant index,view and controlelr

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+restaurants = [
+  { name: "Sushi Zen", address: "Tokyo", phone_number: "08012345678", category: "japanese" },
+  { name: "Pasta Palace", address: "Rome", phone_number: "0634567890", category: "italian" },
+  { name: "Le Baguette", address: "Paris", phone_number: "0145789632", category: "french" },
+  { name: "Dragon Delight", address: "Beijing", phone_number: "0109876543", category: "chinese" },
+  { name: "Waffle World", address: "Brussels", phone_number: "0223456789", category: "belgian" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
